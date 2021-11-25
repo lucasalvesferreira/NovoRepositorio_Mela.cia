9d56e73a9cc0ab13a9e8f82fb855cf62f2f86f81
gugaisaac <linosantanapereira@gmail.com>
2021-11-25 18:23:18 -0300

    Banco de dados arrumado

--- melancia/public/API-MELAN_CIA/app/database.js
+++ melancia/public/API-MELAN_CIA/app/database.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
     host     : 'localhost',
     port     : '3306',
     user     : 'root',
-    password : 'bandtec',
+    password : '',
     database : 'melancia'
 });
 
