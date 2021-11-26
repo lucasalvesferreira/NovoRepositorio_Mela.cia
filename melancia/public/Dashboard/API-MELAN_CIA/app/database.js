a939f60da49886e8bc23b82d96bdbb7f334403d1
unknown <lucas.aferreira@bandtec.com.br>
2021-11-26 02:59:36 -0300

    Lucas-continuaçao dash

--- melancia/public/Dashboard/API-MELAN_CIA/app/database.js
+++ melancia/public/Dashboard/API-MELAN_CIA/app/database.js
@@ -2,9 +2,9 @@ var mysql = require('mysql2');
 var connection = mysql.createConnection({
     host     : 'localhost',
     port     : '3306',
-    user     : 'root',
-    password : '',
-    database : 'melancia'
+    user     : 'urubu',
+    password : 'urubu100',
+    database : 'Sensor'
 });
 
 connection.connect(function(err) {
